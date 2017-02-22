6e9b417fc42a0785bb617130aa772f972ca1226a
Charis Amarantigos <harisamarad@gmail.com>
2017-02-22 14:50:35 +0000

    Fix create and error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,7 @@ class ArticlesController < ApplicationController
   # GET /articles/1
   # GET /articles/1.json
   def show
+    @article = Article.find(params[:id])
   end
 
   # GET /articles/new
