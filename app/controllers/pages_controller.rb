a9b98c4dbcf37315fc5599805f5031b141c65588
Charis Amarantigos <harisamarad@gmail.com>
2017-02-22 11:19:40 +0000

    Create rails application setup home and about pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+
+    def home
+        
+    end
+    
+    def about
+        
+    end
+    
+    def blog
+        
+    end
+    
+    def contact
+        
+    end
+end
